083eab3e6e6e7fd60354c7283dd8fe930c21f317
kras <kras>
2017-08-17 15:23:43 +0300

    Начальные изменения в части заполнения шаблона Word документа. Вместо использования docx обертки python планируется реализация замены с помощью etree (xml) в документе document.xml с помощью след. выражения xpath: //w:r[(preceding-sibling::node()/w:t='{' ) and (following-sibling::node()/w:t='}')]/w:t

--- fill_statement_docx.py
+++ fill_statement_docx.py
@@ -1,26 +1,44 @@
 from docx import Document
 import os
+import zipfile
 
+def docx_replace(old_file,new_file,rep):
+    zin = zipfile.ZipFile (old_file, 'r')
+    zout = zipfile.ZipFile (new_file, 'w')
+    for item in zin.infolist():
+        buffer = zin.read(item.filename)
+        if (item.filename == 'word/document.xml'):
+            res = buffer.decode("utf-8")
+            for r in rep:
+                res = res.replace(r, rep[r])
+            buffer = res.encode("utf-8")
+        zout.writestr(item, buffer)
+    zout.close()
+    zin.close()
 
 def fill_docx(file, path_to_tempalate, path_to_save=os.getcwd(), **kwargs):
     if not os.path.exists(path_to_save):
         os.mkdir(path_to_save)
-    temp = {'number': "<номер>",
-            'name': '<наименование>',
+    temp = {'number': "{номер}",
+            'name': '{наименование}',
             'name_file': '<имя файла>',
             'size': '<размер>'}
-    if os.path.exists(path_to_tempalate):
-        doc = Document(path_to_tempalate)
-        for i in doc.paragraphs:
-            for key in temp.keys():
-                if i.text.find(temp[key]) != -1:
-                    try:
-                        i.text = str(i.text).replace(temp[key], str(kwargs[key]))
-                        doc.save(os.path.join(path_to_save, '{0}.docx'.format(file)))
-                    except KeyError:
-                        print('Не переданы нужные ключи')
-    else:
-        print('путь до шаблона неверный')
+    rep = {temp[d]:kwargs[d] for d in temp.keys()}
+    docx_replace(path_to_tempalate, os.path.join(path_to_save, '{0}.docx'.format(file)), rep=rep)
+    # if os.path.exists(path_to_tempalate):
+    #     doc = Document(path_to_tempalate)
+    #     for i in doc.paragraphs:
+    #         for key in temp.keys():
+    #             if i.text.find(temp[key]) != -1:
+    #                 try:
+    #                     s = i.style
+    #                     i.text = str(i.text).replace(temp[key], str(kwargs[key]))
+    #                     i.style = s
+    #                 except KeyError:
+    #                     print('Не переданы нужные ключи')
+    #     doc.save(os.path.join(path_to_save, '{0}.docx'.format(file)))
+    # else:
+    #     print('путь до шаблона неверный')
 
 
 if __name__ == '__main__':
