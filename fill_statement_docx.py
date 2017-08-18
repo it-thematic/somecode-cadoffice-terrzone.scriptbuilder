8f76a176f2b56cd032a49d197f416d59e29ccdc1
DESKTOP-OHL2LMT\user <kit.angelov@gmail.com>
2017-08-18 16:37:37 +0300

    docx-xml parser

--- fill_statement_docx.py
+++ fill_statement_docx.py
@@ -1,17 +1,29 @@
 from docx import Document
 import os
 import zipfile
+from lxml import etree
+from lxml.etree import QName
 
-def docx_replace(old_file,new_file,rep):
-    zin = zipfile.ZipFile (old_file, 'r')
-    zout = zipfile.ZipFile (new_file, 'w')
+
+def docx_replace(old_file, new_file, rep):
+    zin = zipfile.ZipFile(old_file, 'r')
+    zout = zipfile.ZipFile(new_file, 'w')
     for item in zin.infolist():
         buffer = zin.read(item.filename)
-        if (item.filename == 'word/document.xml'):
-            res = buffer.decode("utf-8")
-            for r in rep:
-                res = res.replace(r, rep[r])
-            buffer = res.encode("utf-8")
+        if item.filename == 'word/document.xml':
+            tree = etree.fromstring(buffer)
+            ns = tree.nsmap
+            it = tree.xpath('//w:r[(preceding-sibling::*/w:t="{") and following-sibling::*/w:t="}"]/w:t', namespaces=ns)
+            bracets = tree.xpath('//w:t', namespaces=ns)
+            for bracet in bracets:
+                if bracet.text in ['{', '}']:
+                    print(bracet.text)
+                    bracet.text = ''
+                    print(bracet.text)
+            for i in it:
+                if i.text in rep.keys():
+                    i.text = rep[i.text]
+            buffer = etree.tostring(tree, pretty_print=True, xml_declaration=True, encoding='utf-8')
         zout.writestr(item, buffer)
     zout.close()
     zin.close()
@@ -20,11 +32,11 @@ def docx_replace(old_file,new_file,rep):
 def fill_docx(file, path_to_tempalate, path_to_save=os.getcwd(), **kwargs):
     if not os.path.exists(path_to_save):
         os.mkdir(path_to_save)
-    temp = {'number': "{номер}",
-            'name': '{наименование}',
-            'name_file': '<имя файла>',
-            'size': '<размер>'}
-    rep = {temp[d]:kwargs[d] for d in temp.keys()}
+    temp = {'number': "номер",
+            'name': 'наименование',
+            'name_file': 'имя файла',
+            'size': 'размер'}
+    rep = {temp[d]: kwargs[d] for d in temp.keys()}
     docx_replace(path_to_tempalate, os.path.join(path_to_save, '{0}.docx'.format(file)), rep=rep)
     # if os.path.exists(path_to_tempalate):
     #     doc = Document(path_to_tempalate)
@@ -44,9 +56,11 @@ def fill_docx(file, path_to_tempalate, path_to_save=os.getcwd(), **kwargs):
 
 if __name__ == '__main__':
     fill_docx(file='first',
-              path_to_tempalate='Заявление в ГКНШаблон.docx',
+              path_to_tempalate='template-doc/Заявление в ГКНШаблон.docx',
               path_to_save='stat_docx',
               number='sdf',
-              name=33,
+              name='33',
               name_file='43',
               size='ee')
+
+    # xpath: //w:r[(preceding-sibling::node()/w:t='{' ) and (following-sibling::node()/w:t='}')]/w:t
